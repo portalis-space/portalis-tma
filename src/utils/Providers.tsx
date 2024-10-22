646bc70842912ef615d04be2cf332e6aa960a125
Gia Endong <giaendong@gmail.com>
2024-10-22 22:46:32 +0700

    chore(wallet): featured evvm wallet

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -31,7 +31,11 @@ const modal = createAppKit({
   metadata: metadata,
   features: {
     analytics: true, // Optional - defaults to your Cloud configuration
-  }
+  },
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // METAMASK
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0' // TRUSTWALLET
+  ]
 })
 // End WalletConnect Wagmi
 
